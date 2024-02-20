1f42d7b3ffccea13a98581cd15e171712d900f62
Shubham Kumar Mandal <114088281+shubhampr07@users.noreply.github.com>
2024-02-20 16:58:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   const [placeList, setPlaceList] = useState([]);
   useEffect(()=>{
-    getPlaceList('Clubs in Bangkok');
+    getPlaceList('Hotels in Dubai');
 
  },[])
 
